e5ab574e7feccc06f932d1efb83449357545b0c9
AaMegaTeam <hamed.dehghani321@gmail.com>
2017-03-23 16:32:03 +0430

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ while [ $COUNTER -lt 5 ]; do
 killall screen
 killall tmux
 
-lua bot.lua # Set Yours (Like : nohup ./launcher2.sh or screen php bot.php or ETC)
+screen -S bot lua bot.lua # Set Yours (Like : nohup ./launcher2.sh or screen php bot.php or ETC)
 
 sleep 5
 
